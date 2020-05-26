03d8999b51d1c8e9743cb14f3cd1830fe1e4d3de
arroyo-piedra <andre.arroyo.piedra@gmail.com>
2020-05-25 18:38:06 -0600

    testing

---
+++ src/testing/another_test.go
@@ -0,0 +1,9 @@
+// +build anothertest
+
+package main
+
+import "testing"
+
+func TestAnother(t *testing.T) {
+	t.Log("All ok")
+}
---
+++ src/testing/code_test.go
@@ -0,0 +1,50 @@
+// +build unit
+
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	x, y := 5, 4
+	t.Logf("Number used: %v , %v", x, y)
+	result := Sum(x, y)
+	if result != 9 {
+		t.Errorf("Should be 9, and find: %v", result)
+	}
+}
+
+var sumTest = []struct {
+	firsNumber   int
+	secondNumber int
+	result       int
+	message      string
+}{
+	{1, 1, 2, "Positive + Positive number ok"},
+	{1, -1, 0, "Positive + Negative number ok"},
+	{-1, -1, -2, "Negative + Negative number ok"},
+}
+
+func TestTableSum(t *testing.T) {
+
+	for _, tt := range sumTest {
+		t.Log(tt.message)
+		result := Sum(tt.firsNumber, tt.secondNumber)
+		if result != tt.result {
+			t.Errorf("Fail, %v + %v = %v", tt.firsNumber, tt.secondNumber, tt.result)
+		}
+	}
+}
+
+func TestRead(t *testing.T) {
+	t.Log("Reading test")
+	data, err := ioutil.ReadFile("data/data.data")
+	if err != nil {
+		t.Fatal("Error trying to open the file")
+	}
+	if string(data) != "It should have this text" {
+		t.Fatal("Info error")
+	}
+}
